d6655641702e79c01dc9a02155fec1cdeff73076
Felix <felix.wensky@gmail.com>
2021-03-18 19:11:50 +0100

    added tdl in class Land added commentary in Test

--- Flaggentrainer/src/Land.java
+++ Flaggentrainer/src/Land.java
@@ -2,14 +2,24 @@ public class Land {
     private String name;
     private int id;
     private String flag;
+    private String TDL;
 
-    Land(String n, int i, String f){
+    Land(String n, int i, String f,String tdl){
         name = n;
         id = i;
         flag = f;
+        TDL = tdl;
     }
+
+
+    public int getId() { return id; }
+
+    public String getFlag() { return flag; }
+
+    public String getTDL() { return TDL; }
 
     public String getName() {
         return name;
     }
+
 }
