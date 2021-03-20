90a3d76d1cfacf0bf1303fac057eef06bb88359a
Felix <felix.wensky@gmail.com>
2021-03-20 19:11:02 +0100

    added class FinishedAlert

---
+++ Flaggentrainer/src/FinishedAlert.java
@@ -0,0 +1,48 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class FinishedAlert {
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> {
+            window.close();
+            FlagGUI.close();
+        });
+
+        Button replayButton = new Button("Replay");
+        replayButton.setOnAction(e -> {
+            window.close();
+            FlagGUI.restart();
+        });
+
+        HBox midlayout = new HBox();
+        midlayout.getChildren().addAll(label);
+        midlayout.setAlignment(Pos.CENTER);
+
+        HBox botlayout = new HBox();
+        botlayout.getChildren().addAll(replayButton, closeButton);
+        botlayout.setAlignment(Pos.CENTER);
+
+        BorderPane bp = new BorderPane();
+        bp.setCenter(midlayout);
+        bp.setBottom(botlayout);
+
+        Scene scene = new Scene(bp, 320, 70);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+}
