1da7fff919728a32663f81da276dc405392577b9
Felix <felix.wensky@gmail.com>
2021-03-25 12:06:05 +0100

    added Landliste.java

---
+++ Flaggentrainer/src/Landliste.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Landliste {
+    private ArrayList<Land> deepList;
+    private ArrayList<Land> workList;
+
+    /**
+     * Override the default constructor
+     * Additional information needs to be added
+     */
+    public Landliste() {
+        deepList = new ArrayList<>();
+        workList = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+
+        InputStream in = getClass().getResourceAsStream("/names.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            while (reader.ready()) {
+                names.add(reader.readLine());
+            }
+            for (int i = 0; i < names.size(); i++) {
+                String n = names.get(i);
+                String[] constructArray = n.split("/"); //aus dem String "name/tdl" wird ein Array [name,tdl]
+                //InputStream flagDir = getClass().getResourceAsStream("/flags/" + constructArray[1] + ".png");
+
+                deepList.add(new Land(constructArray[0], "/flags/" + constructArray[1] + ".png", constructArray[1]));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (Land land : deepList) {
+            workList.add(land.copy());
+        }
+    }
+
+    /**
+     * Reset the Liste
+     */
+    public ArrayList<Land> resetListe(){
+        workList = new ArrayList<>();
+        for (Land land : deepList) {
+            workList.add(land.copy());
+        }
+        return workList;
+    }
+}
